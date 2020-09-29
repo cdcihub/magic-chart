1e9fa7117f7d9d94f1818b7e49d1176e3dfb0284
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-29 18:36:57 +0200

    renamed image

--- make.sh
+++ make.sh
@@ -1,12 +1,15 @@
+function image-tag() {
+    echo "$(cd magic-container; git describe --always)"-"$(cd magic-container/magic-backend; git describe --always)"
+}
 
 function install() {
     set -x
-    helm -n ${NAMESPACE:?} install oda-magic . --set image.tag="$(cd magic-container; git describe --always)"
+    helm -n ${NAMESPACE:?} install oda-magic . --set image.tag=$(image-tag)
 }
 
 function upgrade() {
     set -x
-    helm upgrade -n ${NAMESPACE:?} oda-magic . --set image.tag="$(cd magic-container; git describe --always)"
+    helm upgrade -n ${NAMESPACE:?} oda-magic . --set image.tag=$(image-tag)
 }
 
 $@
